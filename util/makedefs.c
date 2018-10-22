9a6aa80a804c8ade71aa6a22870aee4374e935ad
k21971 <keith.simpson1971@gmail.com>
2018-10-22 14:42:35 -0400

    Change 'NetHack' to 'EvilHack' for version output in-game

--- util/makedefs.c
+++ util/makedefs.c
@@ -1204,7 +1204,7 @@ const char *build_date;
     Strcpy(&subbuf[1], PORT_SUB_ID);
 #endif
 
-    Sprintf(outbuf, "%s NetHack%s Version %s%s - last %s %s.", PORT_ID,
+    Sprintf(outbuf, "%s EvilHack%s Version %s%s - last %s %s.", PORT_ID,
             subbuf, version_string(versbuf, "."), betabuf,
             date_via_env ? "revision" : "build", build_date);
     return outbuf;
