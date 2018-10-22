87423dbfb792302cea1717ba67c44cb3b0032ee7
k21971 <keith.simpson1971@gmail.com>
2018-10-22 00:44:10 -0400

    Welcome message changed from 'NetHack' to 'EvilHack'

--- src/allmain.c
+++ src/allmain.c
@@ -680,8 +680,8 @@ boolean new_game; /* false => restoring an old game */
                 : currentgend != flags.initgend))
         Sprintf(eos(buf), " %s", genders[currentgend].adj);
 
-    pline(new_game ? "%s %s, welcome to NetHack!  You are a%s %s %s."
-                   : "%s %s, the%s %s %s, welcome back to NetHack!",
+    pline(new_game ? "%s %s, welcome to EvilHack!  You are a%s %s %s."
+                   : "%s %s, the%s %s %s, welcome back to EvilHack!",
           Hello((struct monst *) 0), plname, buf, urace.adj,
           (currentgend && urole.name.f) ? urole.name.f : urole.name.m);
 }
