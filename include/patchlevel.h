abde251cf00fd0959021cd1057e32a052bbeacda
k21971 <keith.simpson1971@gmail.com>
2019-01-04 21:25:03 -0500

    Update to version 0.1.2

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -9,7 +9,7 @@
 /*
  * PATCHLEVEL is updated for each release.
  */
-#define PATCHLEVEL 1
+#define PATCHLEVEL 2
 /*
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
@@ -32,7 +32,7 @@
  * PP = patch level, ee = edit level, L = literal suffix "L",
  * with all four numbers specified as two hexadecimal digits.
  */
-#define VERSION_COMPATIBILITY 0x00010000L
+#define VERSION_COMPATIBILITY 0x00010200L
 
 /****************************************************************************/
 /* Version 3.6.x */
