69aa782d963fc6c7ac700d42f918f23d76bc314b
k21971 <keith.simpson1971@gmail.com>
2019-02-22 23:36:25 -0500

    Greater chance of hostile minion appearing when converting an altar. Especially if the altar is in an occupied temple. From SporkHack.

--- src/pray.c
+++ src/pray.c
@@ -1682,9 +1682,11 @@ dosacrifice()
                                                ? NH_BLACK
                                                : (const char *) "gray"));
 
-                    if (rnl(u.ulevel) > 6 && u.ualign.record > 0
-                        && rnd(u.ualign.record) > (3 * ALIGNLIM) / 4)
-                        summon_minion(altaralign, TRUE);
+                    if (u.ualign.record > 0 && rnd(u.ualign.record)
+                        > (3 * ALIGNLIM) / (temple_occupied(u.urooms)
+                        ? 12 : u.ulevel)) {
+			    summon_minion(altaralign, TRUE);
+		    }
                     /* anger priest; test handles bones files */
                     if ((pri = findpriest(temple_occupied(u.urooms)))
                         && !p_coaligned(pri))
