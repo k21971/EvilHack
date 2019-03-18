b5ba9541a91d29fa0d6f3e4e5260c7ce7faea255
k21971 <keith.simpson1971@gmail.com>
2019-03-18 12:40:00 -0400

    Magic markers no longer randomly generated. From SporkHack (sort of) - in SporkHack, magic markers were set as never randomly occuring, and your role would never start with one either. Only way to get one was to either wish for it or (maybe) polypile for one.
    For EvilHack - random generation is gone, as is certain roles spawning with one. However, in Sokoban there's a 50/50 chance of one of the three prizes at the end being a magic marker. I did not change anything in regards to polypiling behavior, so the player could potentially get one via that method.
    
    Another evil bit - wands of nothing are now prices at 500zm just like wands of wishing and death. Have fun price-ID'ing ;)

--- src/u_init.c
+++ src/u_init.c
@@ -177,9 +177,6 @@ static struct trobj Wizard[] = {
 
 static struct trobj Tinopener[] = { { TIN_OPENER, 0, TOOL_CLASS, 1, 0 },
                                     { 0, 0, 0, 0, 0 } };
-static struct trobj Magicmarker[] = { { MAGIC_MARKER, UNDEF_SPE, TOOL_CLASS,
-                                        1, 0 },
-                                      { 0, 0, 0, 0, 0 } };
 static struct trobj Lamp[] = { { OIL_LAMP, 1, TOOL_CLASS, 1, 0 },
                                { 0, 0, 0, 0, 0 } };
 static struct trobj Blindfold[] = { { BLINDFOLD, 0, TOOL_CLASS, 1, 0 },
@@ -672,10 +669,8 @@ u_init()
         ini_inv(Archeologist);
         if (!rn2(10))
             ini_inv(Tinopener);
-        else if (!rn2(4))
+        else if (!rn2(2))
             ini_inv(Lamp);
-        else if (!rn2(10))
-            ini_inv(Magicmarker);
         knows_object(SACK);
         knows_object(TOUCHSTONE);
         skill_init(Skill_A);
@@ -738,9 +733,7 @@ u_init()
 
         Monk[M_BOOK].trotyp = M_spell[rn2(90) / 30]; /* [0..2] */
         ini_inv(Monk);
-        if (!rn2(5))
-            ini_inv(Magicmarker);
-        else if (!rn2(10))
+        if (!rn2(4))
             ini_inv(Lamp);
         knows_class(ARMOR_CLASS);
         /* sufficiently martial-arts oriented item to ignore language issue */
@@ -751,9 +744,7 @@ u_init()
     case PM_PRIEST:
 	if (Race_switch == PM_ELF) Priest[PRI_MACE].trotyp = CLUB;
         ini_inv(Priest);
-        if (!rn2(10))
-            ini_inv(Magicmarker);
-        else if (!rn2(10))
+        if (!rn2(4))
             ini_inv(Lamp);
         knows_object(POT_WATER);
         skill_init(Skill_P);
@@ -797,10 +788,8 @@ u_init()
             ini_inv(Tinopener);
         else if (!rn2(25))
             ini_inv(Leash);
-        else if (!rn2(25))
+        else if (!rn2(5))
             ini_inv(Towel);
-        else if (!rn2(25))
-            ini_inv(Magicmarker);
         skill_init(Skill_T);
         break;
     case PM_VALKYRIE:
@@ -824,7 +813,7 @@ u_init()
     case PM_WIZARD:
         ini_inv(Wizard);
         if (!rn2(5))
-            ini_inv(Magicmarker);
+            ini_inv(Lamp);
         if (!rn2(5))
             ini_inv(Blindfold);
         skill_init(Skill_W);
