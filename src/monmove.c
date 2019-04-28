2172fbb845264a5fba8c2d7e31adc63138fc3152
Tangles <andyrthomson@gmail.com>
2019-04-28 23:33:31 +1000

    minor formatting/indentation fix.

--- src/monmove.c
+++ src/monmove.c
@@ -436,23 +436,23 @@ register struct monst *mtmp;
      * that it's a Boojum, and we need to swap out monsters in that case
      */
     if (mtmp->mnum == PM_SNARK && distu(mtmp->mx,mtmp->my) <= 2) {
-	if (mtmp->m_id % 3 < 1) {
-	    oldx = mtmp->mx; oldy = mtmp->my;
-	    mongone(mtmp);
-	    mdummy = makemon(&mons[PM_BOOJUM], oldx, oldy, NO_MM_FLAGS);
-	    /* If someone has managed to extinct boojum, this will
-	     * result in the monster just vanishing.  But this should
-	     * be fairly difficult to do, since boojum only generate
-	     * from snarks at a rate of one snark, one boojum. */
-	    if (mdummy) {
-		mtmp = mdummy;
-	    if (canseemon(mtmp)) {
-		pline("Oh, no, this Snark is a Boojum!");
-	        }
-	    } else {
-		return 0;	// mtmp just went away, we'd better bail out
-	    }
-	}
+        if (mtmp->m_id % 3 < 1) {
+            oldx = mtmp->mx; oldy = mtmp->my;
+            mongone(mtmp);
+            mdummy = makemon(&mons[PM_BOOJUM], oldx, oldy, NO_MM_FLAGS);
+            /* If someone has managed to extinct boojum, this will
+             * result in the monster just vanishing.  But this should
+             * be fairly difficult to do, since boojum only generate
+             * from snarks at a rate of one snark, one boojum. */
+            if (mdummy) {
+                mtmp = mdummy;
+                if (canseemon(mtmp)) {
+                    pline("Oh, no, this Snark is a Boojum!");
+                }
+            } else {
+                return 0; /* mtmp just went away, we'd better bail out */
+            }
+        }
     }
 
     /* confused monsters get unconfused with small probability */
