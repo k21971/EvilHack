fd91b1e3264c7371dd1373e9077f84f21c69bef7
k21971 <keith.simpson1971@gmail.com>
2019-05-12 13:13:00 -0400

    Update version to 0.3.2 Prepping for some changes/additions that will break saves. This will also be the version that incorporates NetHack 3.6.2 official release.

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -9,7 +9,7 @@
 /*
  * PATCHLEVEL is updated for each release.
  */
-#define PATCHLEVEL 1
+#define PATCHLEVEL 2
 /*
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
@@ -32,7 +32,7 @@
  * PP = patch level, ee = edit level, L = literal suffix "L",
  * with all four numbers specified as two hexadecimal digits.
  */
-#define VERSION_COMPATIBILITY 0x00030100L
+#define VERSION_COMPATIBILITY 0x00030200L
 
 /****************************************************************************/
 /* Version 3.6.x */
