f059a60fd2b47e0a41e636f9112302b49d213be6
k21971 <keith.simpson1971@gmail.com>
2019-08-01 22:17:41 -0400

    Beholders will always show as a 5 via warning. Given what these creatures can do, we'll just hardcode them as always being a 5 via warning instead of jacking up their level so high that you'll rarely encounter them.

--- src/display.c
+++ src/display.c
@@ -527,7 +527,10 @@ struct monst *mon;
 
     if (mon_warning(mon)) {
         tmp = (int) (mon->m_lev / 4);    /* match display.h */
-        wl = (tmp > WARNCOUNT - 1) ? WARNCOUNT - 1 : tmp;
+        if (mon->data == &mons[PM_BEHOLDER])
+            wl = 5;
+        else
+            wl = (tmp > WARNCOUNT - 1) ? WARNCOUNT - 1 : tmp;
     }
     return wl;
 }
