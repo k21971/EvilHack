cd8a80e722fcb4c43f3a2169deb36c149e945bf2
k21971 <keith.simpson1971@gmail.com>
2019-10-07 00:50:34 -0400

    Suppress livelogging of the Red Horse being killed. The Red Horse is the only tame unique monster in this game, it will either spawn tame or not at all. When it dies, it gives the impression the player killed it, when 99 out of 100 times it was killed by another monster. Best to just suppress the livelog of its death altogether.

--- src/mon.c
+++ src/mon.c
@@ -2556,10 +2556,16 @@ register struct monst *mtmp;
     } else if (unique_corpstat(mtmp->data)) {
         switch(mvitals[tmp].died) {
             case 1:
-                livelog_printf(LL_UMONST, "%s %s",
-                    nonliving(mtmp->data) ? "destroyed" : "killed",
-                    noit_mon_nam(mtmp));
-                break;
+                /* don't livelog your unique pet being killed
+                 * by something else, it gives the impression you did it */
+                if (mtmp->data == &mons[PM_RED_HORSE] && mtmp->mtame) {
+                    break;
+                } else {
+                    livelog_printf(LL_UMONST, "%s %s",
+                        nonliving(mtmp->data) ? "destroyed" : "killed",
+                        noit_mon_nam(mtmp));
+                    break;
+                }
             case 5:
             case 10:
             case 50:
