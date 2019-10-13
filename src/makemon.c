3e185ec94ec1ac00a401f0b03407ff6cd35cdc3a
k21971 <keith.simpson1971@gmail.com>
2019-10-13 02:08:23 -0400

    Go back to sleep beholder...

--- src/makemon.c
+++ src/makemon.c
@@ -2130,7 +2130,7 @@ int mmflags;
     case S_JABBERWOCK:
         break;
     case S_EYE:
-        if (mtmp->data != &mons[PM_BEHOLDER]) {
+        if (mtmp->data == &mons[PM_BEHOLDER]) {
             if (rn2(5) && !u.uhave.amulet)
                 mtmp->msleeping = 1;
         }
