804cf637d24ce353f5b64b410ad5a9153645a660
k21971 <keith.simpson1971@gmail.com>
2019-10-16 18:51:13 -0400

    Fix: launchers should not spawn as 'poisoned'.

--- include/obj.h
+++ include/obj.h
@@ -230,6 +230,7 @@ struct obj {
      && objects[otmp->otyp].oc_skill <= -P_BOW)
 #define is_poisonable(otmp)       \
     (otmp->oclass == WEAPON_CLASS \
+     && !is_launcher(otmp)        \
      && objects[otmp->otyp].oc_dir != WHACK)
 #define uslinging() (uwep && objects[uwep->otyp].oc_skill == P_SLING)
 /* 'is_quest_artifact()' only applies to the current role's artifact */
