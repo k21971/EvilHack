6d90810665cc797aa01e036f2da11facdc639382
k21971 <keith.simpson1971@gmail.com>
2019-10-26 02:38:09 -0400

    Fix: variant type title (HTML dumplogs). Noticed whenever an HTML dumplog was being opened for EvilHack, the title showing in the browser tab says NetHack instead of EvilHack. That's misleading, so we'll correct it.

--- src/windows.c
+++ src/windows.c
@@ -1849,9 +1849,9 @@ dump_headers()
 
     fprintf(dumphtml_file, "<!DOCTYPE html>\n");
     fprintf(dumphtml_file, "<head>\n");
-    fprintf(dumphtml_file, "<title>NetHack %s</title>\n",  version_string(vers));
+    fprintf(dumphtml_file, "<title>EvilHack %s</title>\n",  version_string(vers));
     fprintf(dumphtml_file, "<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" />\n");
-    fprintf(dumphtml_file, "<meta name=\"generator\" content=\"NetHack %s \" />\n", vers);
+    fprintf(dumphtml_file, "<meta name=\"generator\" content=\"EvilHack %s \" />\n", vers);
     fprintf(dumphtml_file, "<meta name=\"date\" content=\"%s\" />\n", iso8601);
     fprintf(dumphtml_file, "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\n");
     fprintf(dumphtml_file, "<style type=\"text/css\">\n");
