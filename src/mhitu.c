d189c859566ea4c8da6f1fa1b96326026565f02f
k21971 <keith.simpson1971@gmail.com>
2019-10-31 15:59:37 -0400

    Tweak hard material helmet deflection against attacks to the players head. Now that this was fixed, it may have been a bit too effective. Dialing it back just a tad.

--- src/mhitu.c
+++ src/mhitu.c
@@ -1468,7 +1468,7 @@ register struct attack *mattk;
         /* The material of the helmet on your head determines how effective
          * it will be when deflecting tentacle/bite attacks. Harder material
          * will do a better job than a soft cap. */
-        if (uarmh && is_hard(uarmh) && rn2(5)) {
+        if (uarmh && is_hard(uarmh) && rn2(4)) {
             /* not body_part(HEAD) */
             Your("%s blocks the %s to your head.",
                  helm_simple_name(uarmh), is_zombie(mdat) ? "bite" : "attack");
