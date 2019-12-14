8ec66ea00d2832d57bc73759da5e6a3928f2e026
k21971 <keith.simpson1971@gmail.com>
2019-12-14 14:00:50 -0500

    Fix: objects spawning in unseen shallow pools. The game will crash if the player enters a level where a susceptible object has spawned in a shallow pool or sewage out of sight of the player. For now, reverting a small part of the initial shallow water patch to prevent this from occuring. Side effect of this - the player can drag a non-foodproofed ball & chain behind them through shallow pools and the ball & chain will not rust.

--- src/mkobj.c
+++ src/mkobj.c
@@ -1971,8 +1971,10 @@ int x, y;
         obj_timer_checks(otmp, x, y, 0);
 
     /* for objects initially created in water */
-    if (is_damp_terrain(x, y))
-        water_damage(otmp, FALSE, FALSE);
+    /* disabled until crash fix is found for when objects
+     * spawn in shallow pools not seen by player */
+    /* if (is_damp_terrain(x, y))
+        water_damage(otmp, NULL, FALSE); */
 }
 
 #define ROT_ICE_ADJUSTMENT 2 /* rotting on ice takes 2 times as long */
