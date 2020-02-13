f6af6708f200e679da84781cd4d297f2c3591b7a
k21971 <keith.simpson1971@gmail.com>
2020-02-13 16:55:38 -0500

    Prep for version 0.5.0.

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -5,11 +5,11 @@
 
 /* NetHack 3.6.x */
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 4
+#define VERSION_MINOR 5
 /*
  * PATCHLEVEL is updated for each release.
  */
-#define PATCHLEVEL 2
+#define PATCHLEVEL 0
 /*
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
@@ -32,7 +32,7 @@
  * PP = patch level, ee = edit level, L = literal suffix "L",
  * with all four numbers specified as two hexadecimal digits.
  */
-#define VERSION_COMPATIBILITY 0x00040200L
+#define VERSION_COMPATIBILITY 0x00050000L
 
 /****************************************************************************/
 /* Version 3.6.x */
