7a959bb9792d1166fe2b2723d57cc97eaccf86f3
k21971 <keith.simpson1971@gmail.com>
2020-05-24 02:34:31 +0000

    Fix: impossible() when prison guard tries to initiate a bribe while carrying no gold.

--- src/monmove.c
+++ src/monmove.c
@@ -660,6 +660,11 @@ register struct monst *mtmp;
         long gdemand = 500 * u.ulevel;
         long goffer = 0;
 
+        if (!money_cnt(invent)) { /* can't bribe with no money */
+            mtmp->mspec_used = 1000;
+            return 0;
+        }
+
         pline("%s demands %ld %s to avoid re-arrest.", Amonnam(mtmp),
               gdemand, currency(gdemand));
         if ((goffer = bribe(mtmp)) >= gdemand) {
