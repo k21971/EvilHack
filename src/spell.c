78e112c5cab1a99359682a5bb200aaab97a5c98c
k21971 <keith.simpson1971@gmail.com>
2020-08-24 19:01:32 +0000

    Allow skilled attack spell casters to cast 'basic' level for various attack spells.
    From xNetHack commit 6b357f7, which got it from UnNetHack, which got it
    from dNetHack, which got it from...
    
    This affects the spells fireball, cone of cold, poison blast and acid
    blast - if the caster is skilled or expert in attack spells, casting
    these spells prompts 'Cast advanced spell?' giving the caster the option
    for the advanced effect (area of effect) or the basic effect (beam on a
    single point). If basic effect is chosen, only half of the spell power
    needed to cast it is used.

--- src/spell.c
+++ src/spell.c
@@ -1105,7 +1105,7 @@ boolean atme;
     case SPE_CONE_OF_COLD:
     case SPE_POISON_BLAST:
     case SPE_ACID_BLAST:
-        if (role_skill >= P_SKILLED) {
+        if (role_skill >= P_SKILLED && yn("Cast advanced spell?") == 'y') {
             if (throwspell()) {
                 cc.x = u.dx;
                 cc.y = u.dy;
@@ -1141,7 +1141,11 @@ boolean atme;
                 }
             }
             break;
-        } /* else */
+        } else if (role_skill >= P_SKILLED) {
+            /* player said not to cast advanced spell; return up to half of the
+             * magical energy */
+            u.uen += rnd(energy / 2);
+        }
         /*FALLTHRU*/
 
     /* these spells are all duplicates of wand effects */
