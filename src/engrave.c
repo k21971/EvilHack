18fc06b45d0d1592727b5e91c6c4836ff9aebacf
Michael Meyer <me@entrez.cc>
2020-10-21 19:33:36 -0400

    Skip Elbereth learning check when creating graves
    Because creating a grave when the hero dies involves
    make_engr_at(engrave.c), the check for having learned Elbereth is
    applied when a grave would have Elbereth on it (e.g. because the hero
    was killed by a monster she had named Elbereth, or because the hero
    was named Elbereth). This means that post-death the player receives a
    message about not knowing the word, and the actual engraving on the
    headstone in the bones file is replaced in its entirety with the
    selected Elbereth replacement word.
    
    Skipping this test while creating headstones means this won't happen,
    and graves containing Elbereth will be treated like any other grave.

--- src/engrave.c
+++ src/engrave.c
@@ -443,7 +443,7 @@ xchar e_type;
 
     if ((ep = engr_at(x, y)) != 0)
         del_engr(ep);
-    if (!in_mklev && strstri(s, "Elbereth")) {
+    if (!in_mklev && e_type != HEADSTONE && strstri(s, "Elbereth")) {
         if (!u.uevent.ulearned_elbereth) {
            s = bogus_elbereth[rn2(N_BOGUS_ELBERETH)];
            pline("%s", cannot_write[rn2(N_CANNOT_WRITE)]);
