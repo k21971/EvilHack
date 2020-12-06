898680369f7d9fc5702fa992fc6dbc8bc28ef1dc
k21971 <keith.simpson1971@gmail.com>
2020-12-06 00:11:49 +0000

    Fix: Angelslayer and the Tsurugi of Muramasa were conferring properties on carry when it should have been on wield.

--- include/artilist.h
+++ include/artilist.h
@@ -243,7 +243,7 @@ STATIC_OVL NEARDATA struct artifact artilist[] = {
     /* The quasi-evil twin of Demonbane, Angelslayer is an unholy trident
      * geared towards the destruction of all angelic beings */
     A("Angelslayer", TRIDENT,
-      (SPFX_RESTR | SPFX_WARN | SPFX_ATTK), (SPFX_SEARCH | SPFX_HSPDAM),
+      (SPFX_RESTR | SPFX_WARN | SPFX_ATTK | SPFX_SEARCH | SPFX_HSPDAM), 0,
       MH_ANGEL, FIRE(5, 10), NO_DFNS, NO_CARY, 0, A_CHAOTIC, NON_PM, NON_PM,
       5000L, NO_COLOR),
     /* Yeenoghu's infamous triple-headed flail, also known as 'Butcher'.
@@ -355,7 +355,7 @@ STATIC_OVL NEARDATA struct artifact artilist[] = {
 
     A("The Tsurugi of Muramasa", TSURUGI,
       (SPFX_NOGEN | SPFX_RESTR | SPFX_INTEL | SPFX_BEHEAD | SPFX_LUCK
-       | SPFX_PROTECT), SPFX_HPHDAM, 0,
+       | SPFX_PROTECT | SPFX_HPHDAM), 0, 0,
       PHYS(3, 8), NO_DFNS, NO_CARY, 0, A_LAWFUL, PM_SAMURAI, NON_PM,
       6000L, NO_COLOR),
 
