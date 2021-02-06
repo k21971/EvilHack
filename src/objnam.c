a1cf5d44139251d4049427a78077a9a88287629a
k21971 <keith.simpson1971@gmail.com>
2021-02-06 18:31:56 +0000

    Fix: instances where article was missing from the Bag of the Hesperides.
    Good catch qt, thanks for providing a quick fix.
    
    Co-authored-by: entrez <me@entrez.cc>

--- src/objnam.c
+++ src/objnam.c
@@ -2056,7 +2056,8 @@ const char *verb;
     /* leave off "your" for most of your artifacts, but prepend
      * "your" for unique objects and "foo of bar" quest artifacts */
     if (!carried(obj) || !obj_is_pname(obj)
-        || obj->oartifact >= ART_XIUHCOATL) {
+        || obj->oartifact >= ART_XIUHCOATL
+        || obj->oartifact == ART_BAG_OF_THE_HESPERIDES) {
         char *outbuf = shk_your(nextobuf(), obj);
         int space_left = BUFSZ - 1 - strlen(outbuf);
 
@@ -2137,7 +2138,8 @@ struct obj *obj;
     /* leave off "your" for most of your artifacts, but prepend
      * "your" for unique objects and "foo of bar" quest artifacts */
     if (!carried(obj) || !obj_is_pname(obj)
-        || obj->oartifact >= ART_XIUHCOATL) {
+        || obj->oartifact >= ART_XIUHCOATL
+        || obj->oartifact == ART_BAG_OF_THE_HESPERIDES) {
         char *outbuf = shk_your(nextobuf(), obj);
         int space_left = BUFSZ - 1 - strlen(outbuf);
 
