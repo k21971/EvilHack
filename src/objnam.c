f2725ea88a965c868e51692789d6a97ad5289613
k21971 <keith.simpson1971@gmail.com>
2021-02-16 13:44:32 +0000

    Fix: another missing article with an artifact.
    Magic 8-ball.

--- src/objnam.c
+++ src/objnam.c
@@ -2016,7 +2016,8 @@ const char *verb;
      * "your" for unique objects and "foo of bar" quest artifacts */
     if (!carried(obj) || !obj_is_pname(obj)
         || obj->oartifact >= ART_XIUHCOATL
-        || obj->oartifact == ART_BAG_OF_THE_HESPERIDES) {
+        || obj->oartifact == ART_BAG_OF_THE_HESPERIDES
+        || obj->oartifact == ART_MAGIC___BALL) {
         char *outbuf = shk_your(nextobuf(), obj);
         int space_left = BUFSZ - 1 - strlen(outbuf);
 
@@ -2098,7 +2099,8 @@ struct obj *obj;
      * "your" for unique objects and "foo of bar" quest artifacts */
     if (!carried(obj) || !obj_is_pname(obj)
         || obj->oartifact >= ART_XIUHCOATL
-        || obj->oartifact == ART_BAG_OF_THE_HESPERIDES) {
+        || obj->oartifact == ART_BAG_OF_THE_HESPERIDES
+        || obj->oartifact == ART_MAGIC___BALL) {
         char *outbuf = shk_your(nextobuf(), obj);
         int space_left = BUFSZ - 1 - strlen(outbuf);
 
