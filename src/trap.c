41684df3f9d445de5d1ee394988e49d28eb822ee
k21971 <keith.simpson1971@gmail.com>
2021-03-05 19:24:39 +0000

    Even more tweaks to last commit (drawbridge).

--- src/trap.c
+++ src/trap.c
@@ -3386,8 +3386,8 @@ long hmask, emask; /* might cancel timeout */
                                                                : ICED_MOAT;
                 lev->typ = ICE;
             }
-            if (lev->icedpool != ICED_PUDDLE
-                || lev->icedpool != ICED_SEWAGE)
+            if (!(lev->icedpool == ICED_PUDDLE
+                  || lev->icedpool == ICED_SEWAGE))
                 bury_objs(u.ux, u.uy);
             pline("The %s crackles and freezes under your feet.",
                   hliquid(is_sewage(u.ux, u.uy) ? "sewage" : "water"));
@@ -6150,14 +6150,13 @@ lava_effects()
 
     if (how_resistant(FIRE_RES) < 100) {
         if (Wwalking) {
-	    if (uarm && (uarm->otyp == WHITE_DRAGON_SCALE_MAIL
+            if (uarm && (uarm->otyp == WHITE_DRAGON_SCALE_MAIL
                          || uarm->otyp == WHITE_DRAGON_SCALES)) {
-		levl[u.ux][u.uy].typ = ROOM;
-		if (!rn2(4)) {
-	            pline_The("lava cools and solidifies under your feet.");
-		}
-		return TRUE;
-	    }
+                levl[u.ux][u.uy].typ = ROOM;
+                if (!rn2(4))
+                    pline_The("lava cools and solidifies under your feet.");
+                return TRUE;
+            }
             pline_The("%s here burns you!", hliquid("lava"));
             if (usurvive) {
                 losehp(dmg, lava_killer, KILLED_BY); /* lava damage */
