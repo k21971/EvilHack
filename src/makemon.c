4a79e1e6b613c388914191e50adee61839858dd9
k21971 <keith.simpson1971@gmail.com>
2021-10-19 16:12:17 +0000

    Correct typo from last commit.

--- src/makemon.c
+++ src/makemon.c
@@ -3525,7 +3525,7 @@ register struct monst *mtmp;
         return FALSE;
 
     /* monsters of same race as the player tend to be peaceful,
-       followers of Moloch ar ethe exception */
+       followers of Moloch and Convicts are the exception */
     if (erac_race_peaceful(mtmp) && ual != A_NONE && !Role_if(PM_CONVICT))
         return TRUE;
     if (erac_race_hostile(mtmp))
