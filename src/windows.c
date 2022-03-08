45921514356466198774610343666fe783dd5275
k21971 <keith.simpson1971@gmail.com>
2022-03-08 14:46:37 +0000

    Fix: silence a few dumphtml unused variable warnings.
    Would occur if compiling without defining DUMPHTML. Thanks aosdict.
    
    Co-authored-by: copperwater <copperwater@users.noreply.github.com>

--- src/windows.c
+++ src/windows.c
@@ -1454,6 +1454,10 @@ boolean onoff;
         if (attrmask & HL_BOLD)
             fprintf(dumphtml_file, BOLD_E);
     }
+#else
+    nhUse(coloridx);
+    nhUse(attrmask);
+    nhUse(onoff);
 #endif
 }
 
