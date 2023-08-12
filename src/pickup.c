d10126cbf8a82f50dd15977f4a03cb6fda75a13c
saltwaterterrapin@gmail.com <saltwaterterrapin@gmail.com>
2023-08-11 21:27:02 -0400

    Fix: Safes don't have lids "The iron lid hurts to touch" becomes "The iron safe door hurts to touch" in the case of safes.

--- src/pickup.c
+++ src/pickup.c
@@ -1803,7 +1803,8 @@ int cindex, ccount; /* index of this container (1..N), number of them (N) */
 
     if (Hate_material(cobj->material) && !uarmg) {
         char kbuf[BUFSZ];
-        pline("The %s lid %s!", materialnm[cobj->material],
+        pline("The %s %s %s!", materialnm[cobj->material],
+              cobj->otyp == IRON_SAFE ? "safe door" : Is_box(cobj) ? "lid" : "container",
               cobj->material == SILVER ? "sears your flesh" : "hurts to touch");
         Sprintf(kbuf, "opening a %s container", materialnm[cobj->material]);
         losehp(rnd(sear_damage(cobj->material)), kbuf, KILLED_BY);
