k21971/EvilHack

commit fb408017ac4bd02a132efb836339c0bc1c7618d1

Fix: player could drop a cursed loadstone by wielding it.

src/do.c
@@ -741,7 +741,7 @@ const char *word;
             Norep("You cannot %s %s you are wearing.", word, something);
         return FALSE;
     }
-    if (obj->otyp == LOADSTONE && cursed(obj, TRUE)) {
+    if (obj->otyp == LOADSTONE && obj->cursed) {
         /* getobj() kludge sets corpsenm to user's specified count
            when refusing to split a stack of cursed loadstones */
         if (*word) {

commit 68087a2fe3f411053b044c0aa594a971c65dbc74

Fix: Infidels couldn't drop cursed loadstones.
Fixes the issue that was accidentally introduced in commit fb40801.

src/do.c
@@ -742,7 +742,8 @@ const char *word;
             Norep("You cannot %s %s you are wearing.", word, something);
         return FALSE;
     }
-    if (obj->otyp == LOADSTONE && obj->cursed) {
+    if (obj->otyp == LOADSTONE
+        && obj->cursed && !Role_if(PM_INFIDEL)) {
         /* getobj() kludge sets corpsenm to user's specified count
            when refusing to split a stack of cursed loadstones */
         if (*word) {