k21971/EvilHack

commit ad1ddb8fb6ddedf08c74aaad6fe89d3618363f3f

Element vulnerabilities. From SporkHack - certain spell-casting monsters have a new clerical spell at their disposal, which can temporarily make you more vulnerable to damage caused by either fire, cold, electricity or acid. Whatever percentage resistance you have to any of these is decreased by 50%.
This doesn't just affect the player - certain monsters are inherently vulnerable to certain elemental attacks as well (damage is 1.5x of normal). See src/monst.c for changes. Some obvious ones would be: winter wolves being vulnerable to fire, and hell hounds being vulnerable to cold attacks.

src/mthrowu.c
@@ -448,6 +448,7 @@ int range;          /* how much farther will object travel if it misses;
 boolean verbose;    /* give message(s) even when you can't see what happened */
 {
     int damage, tmp;
+    int damtype = AD_PHYS;
     boolean vis, ismimic;
     int objgone = 1;
     struct obj *mon_launcher = archer ? MON_WEP(archer) : NULL;
@@ -511,6 +512,7 @@ boolean verbose;    /* give message(s) even when you can't see what happened */
                     pline_The("poison doesn't seem to affect %s.",
                               mon_nam(mtmp));
             } else {
+                damtype = AD_DRST;
                 if (rn2(30)) {
                     damage += rnd(6);
                 } else {
@@ -532,6 +534,7 @@ boolean verbose;    /* give message(s) even when you can't see what happened */
                 if (vis || (verbose && !target))
                     pline("%s is unaffected.", Monnam(mtmp));
             } else {
+                damtype = AD_ACID;
                 if (vis)
                     pline_The("%s burns %s!", hliquid("acid"), mon_nam(mtmp));
                 else if (verbose && !target)
@@ -546,7 +549,7 @@ boolean verbose;    /* give message(s) even when you can't see what happened */
         }
 
         if (!DEADMONSTER(mtmp)) { /* might already be dead (if petrified) */
-            mtmp->mhp -= damage;
+            damage_mon(mtmp, damage, AD_ACID);
             if (DEADMONSTER(mtmp)) {
                 if (vis || (verbose && !target))
                     pline("%s is %s!", Monnam(mtmp),

include/extern.h
@@ -1457,6 +1457,7 @@ E boolean FDECL(angry_guards, (BOOLEAN_P));
 E void NDECL(pacify_guards);
 E void FDECL(decide_to_shapeshift, (struct monst *, int));
 E boolean FDECL(vamp_stone, (struct monst *));
+E boolean FDECL(damage_mon, (struct monst*, int, int));
 
 /* ### mondata.c ### */
 
@@ -1468,8 +1469,9 @@ E boolean FDECL(poly_when_stoned, (struct permonst *));
 E boolean FDECL(resists_drli, (struct monst *));
 E boolean FDECL(resists_magm, (struct monst *));
 E boolean FDECL(resists_blnd, (struct monst *));
-E boolean
-FDECL(can_blnd, (struct monst *, struct monst *, UCHAR_P, struct obj *));
+E boolean FDECL(vulnerable_to, (struct monst *, int));
+E boolean FDECL(can_blnd, (struct monst *, struct monst *,
+                           UCHAR_P, struct obj *));
 E boolean FDECL(ranged_attk, (struct permonst *));
 E boolean FDECL(hates_silver, (struct permonst *));
 E boolean FDECL(mon_hates_silver, (struct monst *));

include/monflag.h
@@ -150,8 +150,13 @@
 #define M3_INFRAVISIBLE 0x0200 /* visible by infravision */
 
 #define M3_DISPLACES 0x0400 /* moves monsters out of its way */
-#define M3_SKITTISH  0x0800 /* tends to stay a few squares away from you */
-#define M3_ACCURATE  0x1000 /* gets an accuracy bonus to hit */
+#define M3_SKITTISH 0x0800  /* tends to stay a few squares away from you */
+#define M3_ACCURATE 0x1000  /* gets an accuracy bonus to hit */
+
+#define M4_VULNERABLE_FIRE 0x0001 /* inherent elemental vulnerabilities */
+#define M4_VULNERABLE_COLD 0x0002
+#define M4_VULNERABLE_ELEC 0x0004
+#define M4_VULNERABLE_ACID 0x0008
 
 #define MZ_TINY 0          /* < 2' */
 #define MZ_SMALL 1         /* 2-4' */

include/permonst.h
@@ -58,6 +58,7 @@ struct permonst {
     unsigned long mflags1,      /* boolean bitflags */
         mflags2;                /* more boolean bitflags */
     unsigned short mflags3;     /* yet more boolean bitflags */
+    unsigned short mflags4;     /* yup. another boolean bitflag */
     unsigned long mhflags;      /* race boolean bitflags */
     uchar difficulty;		/* toughness (formerly from  makedefs -m) */
 #ifdef TEXTCOLOR

include/prop.h
@@ -86,9 +86,13 @@ enum prop_types {
     REFLECTING        = 66,
     FREE_ACTION       = 67,
     FIXED_ABIL        = 68,
-    LIFESAVED         = 69
+    LIFESAVED         = 69,
+    VULN_FIRE         = 70,
+    VULN_COLD         = 71,
+    VULN_ELEC         = 72,
+    VULN_ACID         = 73
 };
-#define LAST_PROP (LIFESAVED)
+#define LAST_PROP (VULN_ACID)
 
 /*** Where the properties come from ***/
 /* Definitions were moved here from obj.h and you.h */

include/youprop.h
@@ -52,6 +52,26 @@
 #define EDrain_resistance u.uprops[DRAIN_RES].extrinsic
 #define Drain_resistance (HDrain_resistance || EDrain_resistance)
 
+#define HVulnerable_fire u.uprops[VULN_FIRE].intrinsic
+#define EVulnerable_fire u.uprops[VULN_FIRE].extrinsic
+#define Vulnerable_fire (HVulnerable_fire || EVulnerable_fire \
+		         || vulnerable_to(&youmonst, AD_FIRE))
+
+#define HVulnerable_cold u.uprops[VULN_COLD].intrinsic
+#define EVulnerable_cold u.uprops[VULN_COLD].extrinsic
+#define Vulnerable_cold (HVulnerable_cold || EVulnerable_cold \
+		         || vulnerable_to(&youmonst, AD_COLD))
+
+#define HVulnerable_elec u.uprops[VULN_ELEC].intrinsic
+#define EVulnerable_elec u.uprops[VULN_ELEC].extrinsic
+#define Vulnerable_elec (HVulnerable_elec || EVulnerable_elec \
+		         || vulnerable_to(&youmonst, AD_ELEC))
+
+#define HVulnerable_acid u.uprops[VULN_ACID].intrinsic
+#define EVulnerable_acid u.uprops[VULN_ACID].extrinsic
+#define Vulnerable_acid	(HVulnerable_acid || EVulnerable_acid \
+		         || vulnerable_to(&youmonst, AD_ACID))
+
 /* Hxxx due to FROMFORM only */
 #define HAntimagic u.uprops[ANTIMAGIC].intrinsic
 #define EAntimagic u.uprops[ANTIMAGIC].extrinsic

src/apply.c
@@ -754,7 +754,7 @@ register xchar x, y;
                 ; /* still close enough */
             } else if (otmp->cursed && !breathless(mtmp->data)) {
                 if (um_dist(mtmp->mx, mtmp->my, 5)
-                    || (mtmp->mhp -= rnd(2)) <= 0) {
+                    || damage_mon(mtmp, rnd(2), AD_PHYS)) {
                     long save_pacifism = u.uconduct.killer;
 
                     Your("leash chokes %s to death!", mon_nam(mtmp));

src/cmd.c
@@ -2598,6 +2598,14 @@ int final;
                 from_what(HALLUC_RES));
     if (u.uedibility)
         you_can("recognize detrimental food", "");
+    if (Vulnerable_fire)
+        you_are("vulnerable to fire", "");
+    if (Vulnerable_cold)
+        you_are("vulnerable to cold", "");
+    if (Vulnerable_elec)
+        you_are("vulnerable to electricity", "");
+    if (Vulnerable_acid)
+        you_are("vulnerable to acid", "");
 
     /*** Vision and senses ***/
     if (!Blind && (Blinded || !haseyes(youmonst.data)))

commit 80f7d2ab650f4206f0885148cada2d91d0767c18

Fix: thrown/projectile damage monster vs monster.
A nearly six year old bug, has existed since version 0.5.0, but for
whatever reason was not really noticed until just recently. Monsters
attacking other monsters with thrown/fired projectiles, the target
monster was not taking any damage. Commit ad1ddb8 introduced this bug
with the inclusion of damage_mon() function. The section of code in
ohitmon() in mthrowu.c that handles regular projectiles, I
unknowingly had it handle just acid damage instead. Oops.

src/mthrowu.c
@@ -562,7 +562,7 @@ boolean verbose;    /* give message(s) even when you can't see what happened */
         damage = dmgval(otmp, mtmp);
         if (otmp->otyp == ACID_VENOM
             && (resists_acid(mtmp) || defended(mtmp, AD_ACID)))
-            damage = 0;
+            damage = 0; /* feedback handled further down */
 #if 0 /* can't use this because we don't have the attacker */
         if (is_orc(mtmp->data) && is_elf(?magr?))
             damage++;
@@ -650,9 +650,8 @@ boolean verbose;    /* give message(s) even when you can't see what happened */
                 damage = 0;
         }
 
-        if (!DEADMONSTER(mtmp)
-            && otmp->otyp == ACID_VENOM) { /* might already be dead (if petrified) */
-            damage_mon(mtmp, damage, AD_ACID);
+        if (!DEADMONSTER(mtmp)) { /* might already be dead (if petrified) */
+            mtmp->mhp -= damage;
             if (DEADMONSTER(mtmp)) {
                 if (vis || (verbose && !target))
                     pline("%s is %s!", Monnam(mtmp),

src/weapon.c
@@ -366,7 +366,8 @@ struct monst *mon;
     struct permonst *ptr = r_data(mon);
     boolean Is_weapon = (otmp->oclass == WEAPON_CLASS || is_weptool(otmp));
 
-    if (!ptr) ptr = &mons[NUMMONS];
+    if (!ptr)
+        ptr = &mons[NUMMONS];
 
     if (otyp == CREAM_PIE)
         return 0;