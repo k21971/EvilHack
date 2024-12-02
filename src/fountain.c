k21971/EvilHack

commit b0ee24d75375b8238666c6e316c15781bca6c72a

Fix: player being blamed for monster spell drying up a fountain. Specifically the monster spell 'fire blast'.

src/fountain.c
@@ -208,7 +208,7 @@ boolean isyou;
                 if (DEADMONSTER(mtmp))
                     continue;
                 if (is_watch(mtmp->data) && couldsee(mtmp->mx, mtmp->my)
-                    && mtmp->mpeaceful) {
+                    && mtmp->mpeaceful && !MON_CASTBALL) {
                     if (!Deaf) {
                         pline("%s yells:", Amonnam(mtmp));
                         verbalize("Hey, stop using that fountain!");
@@ -242,7 +242,7 @@ boolean isyou;
            or felt if the hero is blind. */
         newsym(x, y);
         level.flags.nfountains--;
-        if (isyou && in_town(x, y))
+        if (isyou && in_town(x, y) && !MON_CASTBALL)
             (void) angry_guards(FALSE);
     }
 }

commit b93ef857bc2f905b8b51c050f0fdd91ee42a8265

Fix: town guards not becoming angry when fountain is used up.
Another old, self-induced bug. Was reported that the town watch would
never get angry when the player used up a fountain. Commit b0ee24d broke
this while attempting to fix the issue of the player being blamed for a
monster drying up a fountain by casting the monster spell 'fire bolt' or
a flaming sphere exploding near it.

To fix: revert that old commit, and fix the way blame is dished out
properly. So kinda fixing two issues at once here.

src/fountain.c
@@ -207,7 +207,7 @@ boolean isyou;
                 if (DEADMONSTER(mtmp))
                     continue;
                 if (is_watch(mtmp->data) && couldsee(mtmp->mx, mtmp->my)
-                    && mtmp->mpeaceful && !MON_CASTBALL) {
+                    && mtmp->mpeaceful) {
                     if (!Deaf) {
                         pline("%s yells:", Amonnam(mtmp));
                         verbalize("Hey, stop using that fountain!");
@@ -241,7 +241,7 @@ boolean isyou;
            or felt if the hero is blind. */
         newsym(x, y);
         level.flags.nfountains--;
-        if (isyou && in_town(x, y) && !MON_CASTBALL)
+        if (isyou && in_town(x, y))
             (void) angry_guards(FALSE);
     }
 }

src/zap.c
@@ -5602,7 +5602,7 @@ boolean moncast;
             if (see_it)
                 pline("Steam billows from the fountain.");
             rangemod -= 1;
-            dryup(x, y, type > 0);
+            dryup(x, y, moncast ? FALSE : TRUE);
         } else if (IS_PUDDLE(lev->typ) || IS_SEWAGE(lev->typ)) {
             if (see_it) {
                 if (IS_PUDDLE(lev->typ))