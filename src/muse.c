k21971/EvilHack

commit a9ce370232277e18c51d2c39c8f8fa902da0f194

Monsters will use a wand of undead turning directly against the player if they are Draugr or poly'd into an undead monster.
Previously, monsters would only zap a wand of undead turning at you if
you were wielding something like a cockatrice corpse, or had a lizard in
open inventory. Since we now have an actual undead player race, it makes
sense for intelligent monsters to use it offensively against the player
directly if they are Draugr, or poly'd into an undead creature.

src/muse.c
@@ -540,12 +540,13 @@ struct monst *mtmp;
         && lined_up(mtmp)) { /* only lines up if distu range is within 5*5 */
         /* could use m_carrying(), then nxtobj() when matching wand
            is empty, but direct traversal is actually simpler here */
-        for (obj = mtmp->minvent; obj; obj = obj->nobj)
+        for (obj = mtmp->minvent; obj; obj = obj->nobj) {
             if (obj->otyp == WAN_UNDEAD_TURNING && obj->spe > 0) {
                 m.defensive = obj;
                 m.has_defense = MUSE_WAN_UNDEAD_TURNING;
                 return TRUE;
             }
+        }
     }
 
     fraction = u.ulevel < 10 ? 5 : u.ulevel < 14 ? 4 : 3;
@@ -1503,6 +1504,11 @@ struct obj *obj;
          */
         m.offensive = obj;
         m.has_offense = MUSE_WAN_UNDEAD_TURNING;
+    } else if (Race_if(PM_DRAUGR) || is_undead(youmonst.data)) {
+        /* player is Draugr race or poly'd into an undead
+           monster */
+        m.offensive = obj;
+        m.has_offense = MUSE_WAN_UNDEAD_TURNING;
     }
 }
 
@@ -1711,7 +1717,10 @@ boolean reflection_skip;
         }
 
         nomore(MUSE_WAN_UNDEAD_TURNING);
-        m_use_undead_turning(mtmp, obj);
+        {
+            m_use_undead_turning(mtmp, obj);
+            continue;
+        }
 
         nomore(MUSE_WAN_CANCELLATION);
         if (obj->otyp == WAN_CANCELLATION) {

src/zap.c
@@ -1293,9 +1293,11 @@ unturn_you()
     (void) unturn_dead(&youmonst); /* hit carried corpses and eggs */
 
     if (maybe_polyd(is_undead(youmonst.data), Race_if(PM_DRAUGR))) {
-        if (!(Stun_resistance || wielding_artifact(ART_TEMPEST)))
+        if (!(Stun_resistance || wielding_artifact(ART_TEMPEST))) {
             You_feel("frightened and %sstunned.", Stunned ? "even more " : "");
-        make_stunned((HStun & TIMEOUT) + (long) rnd(30), FALSE);
+            make_stunned((HStun & TIMEOUT) + (long) rnd(30), FALSE);
+        }
+        losehp(d(4, 4), "wand of undead turning", KILLED_BY_AN);
     } else {
         You("shudder in dread.");
     }

commit 8ecfc0fe16e7d5678706896c0e8219922f576115

Fix: monsters stopped using potions offensively.
Commit a9ce370 broke this (0.9.0), dunno what I was thinking there
wrapping m_use_undead_turning() in brackets like that. Everything below
it, monsters would no longer use offensively. So all potions, scrolls of
charging and stinking cloud, and wands of striking, cancellation, slow
monster, teleportation, and polymorph. The commit title mentions
potions, as that is what was noticed, but this bug affected a lot more.
I inadvertently made version 0.9.0 easy mode 😀

src/muse.c
@@ -1719,10 +1719,7 @@ boolean reflection_skip;
         }
 
         nomore(MUSE_WAN_UNDEAD_TURNING);
-        {
-            m_use_undead_turning(mtmp, obj);
-            continue;
-        }
+        m_use_undead_turning(mtmp, obj);
 
         nomore(MUSE_WAN_CANCELLATION);
         if (obj->otyp == WAN_CANCELLATION) {
@@ -1895,7 +1892,7 @@ boolean reflection_skip;
         m.offensive = charge_scroll;
         m.has_offense = MUSE_SCR_CHARGING;
     }
-    return(boolean) !!m.has_offense;
+    return (boolean) !!m.has_offense;
 #undef nomore
 #undef pick_to_charge
 }
@@ -2473,7 +2470,7 @@ struct monst *mtmp;
             if (minvptr == otmp && otmp->lamplit)
                 end_burn(otmp, TRUE);
         }
-        return 2;
+        return (DEADMONSTER(mtmp)) ? 1 : 2;
     }
     case MUSE_SCR_STINKING_CLOUD:
         mreadmsg(mtmp, otmp);
@@ -2482,7 +2479,7 @@ struct monst *mtmp;
         (void) create_gas_cloud(mtmp->mux, mtmp->muy, 3 + bcsign(otmp),
                                 8 + 4 * bcsign(otmp));
         m_useup(mtmp, otmp);
-        return 2;
+        return (DEADMONSTER(mtmp)) ? 1 : 2;
     case 0:
         return 0; /* i.e. an exploded wand */
     default:
@@ -3001,7 +2998,7 @@ struct obj *start;
         m.has_misc = 0;
         return FALSE;
     }
-    return(boolean) !!m.has_misc;
+    return (boolean) !!m.has_misc;
 #undef nomore
 #undef pick_to_charge
 }