k21971/EvilHack

commit aea1e4156ba1fea85f5cc9f111a405671cc0231b

Fix: allow burning hands and shocking grasp spells to function while polymorphed; monster attack/body part tweaks.
It was discovered that there was no handling for burning hands/shocking
grasp spells while polymorphed. This commit allows those spells to
function now while polymorphed, however the new forms attack has to be
either a claw, touch, or kick (forehooves) attack.

While working on this, I noticed that feedback for birds attacking with
their forelimbs would display 'wings' instead of claws, so I simply
forced the feedback to display 'claws' for birds.

Also while working on this, I noticed that several quadruped monsters
had the wrong kind of attacks (which in turn was giving incorrect attack
feedback). Rothes, leocrotta, titanotheres, and baluchitheria have had
their claw attacks changed to kicks, rothes had one of their bite
attacks switched to head butt (rothes are basically bison). This in turn
provides proper attack feedback - and these quadrupeds technically
trample you instead of kicking, so that feedback is present. Damage
values were unchanged.

src/uhitm.c
@@ -1415,7 +1415,8 @@ int dieroll;
                          && !Upolyd && Race_if(PM_DROW))
                     && mon_hates_light(mon))
                     lightobj = TRUE;
-                if ((u.usteed || Race_if(PM_CENTAUR)) && !thrown && tmp > 0
+                if ((u.usteed || (!Upolyd && Race_if(PM_CENTAUR)))
+                    && !thrown && tmp > 0
                     && weapon_type(obj) == P_LANCE && mon != u.ustuck) {
                     jousting = joust(mon, obj);
                     /* exercise skill even for minimal damage hits */
@@ -4349,7 +4350,7 @@ boolean weapon_attacks; /* skip weapon attacks if false */
                     verb = "tentacles";
                     break;
                 case AT_KICK:
-                    verb = "kick";
+                    verb = is_quadruped(youmonst.data) ? "trample" : "kick";
                     break;
                 case AT_BUTT:
                     verb = (has_trunk(youmonst.data)
@@ -4383,6 +4384,101 @@ boolean weapon_attacks; /* skip weapon attacks if false */
                             searmsg(&youmonst, mon, hated_obj, FALSE);
                     }
                     sum[i] = damageum(mon, mattk, specialdmg);
+                }
+                /* burning hands spell */
+                if (u.umburn && !DEADMONSTER(mon)
+                    && (mattk->aatyp == AT_CLAW
+                        || mattk->aatyp == AT_TUCH || mattk->aatyp == AT_KICK)) {
+                    int enchant_skill = ((P_SKILL(P_ENCHANTMENT_SPELL) >= P_EXPERT)
+                                         ? 4 : (P_SKILL(P_ENCHANTMENT_SPELL) == P_SKILLED)
+                                           ? 3 : (P_SKILL(P_ENCHANTMENT_SPELL) == P_BASIC)
+                                             ? 2 : 1);
+                    int burning_hands = damage_mon(mon, d(enchant_skill, 4), AD_FIRE);
+
+                    if (resists_fire(mon) || defended(mon, AD_FIRE)
+                        || mon_underwater(mon)) {
+                        shieldeff(mon->mx, mon->my);
+                        golemeffects(mon, AD_FIRE, tmp);
+                        if (!Blind)
+                            Your("%s don't burn %s!",
+                                 is_bird(youmonst.data)
+                                   ? "claws" : makeplural(body_part(HAND)),
+                                 mon_nam(mon));
+                    } else {
+                        You("%s %s with your %s!",
+                            can_vaporize(mon->data)
+                              ? "vaporize part of"
+                              : vulnerable_to(mon, AD_FIRE)
+                                ? "severely burn" : "burn",
+                            mon_nam(mon),
+                            is_bird(youmonst.data)
+                              ? "claws" : makeplural(body_part(HAND)));
+
+                        if (!rn2(4) &&
+                            (mon_underwater(mon) || can_vaporize(mon->data))) {
+                            u.umburn = 0;
+                            Your("%s are extinguished.",
+                                 is_bird(youmonst.data)
+                                   ? "claws" : makeplural(body_part(HAND)));
+                        }
+
+                        if (!rn2(4))
+                            (void) destroy_mitem(mon, POTION_CLASS, AD_FIRE);
+                        if (!rn2(4))
+                            (void) destroy_mitem(mon, SCROLL_CLASS, AD_FIRE);
+                        if (!rn2(7))
+                            (void) destroy_mitem(mon, SPBOOK_CLASS, AD_FIRE);
+
+                        if (completelyburns(mon->data)
+                            || mon->data == &mons[PM_WOOD_GOLEM]
+                            || mon->data == &mons[PM_GREEN_SLIME]
+                            || (is_blight(mon->data) && !rn2(10))
+                            || (mon->data == &mons[PM_ENT] && !rn2(10))
+                            || (mon->data == &mons[PM_ELDER_ENT] && !rn2(12))) {
+                            pline("%s ignites and turns to ash!", Monnam(mon));
+                            killed(mon);
+                        } else {
+                            sum[i] = damageum(mon, mattk, burning_hands);
+                        }
+                    }
+                    nohandburn();
+                }
+                /* shocking grasp spell */
+                if (u.umshock && !DEADMONSTER(mon)
+                    && (mattk->aatyp == AT_CLAW
+                        || mattk->aatyp == AT_TUCH || mattk->aatyp == AT_KICK)) {
+                    int enchant_skill = ((P_SKILL(P_ENCHANTMENT_SPELL) >= P_EXPERT)
+                                         ? 4 : (P_SKILL(P_ENCHANTMENT_SPELL) == P_SKILLED)
+                                           ? 3 : (P_SKILL(P_ENCHANTMENT_SPELL) == P_BASIC)
+                                             ? 2 : 1);
+                    int shocking_grasp = damage_mon(mon, d(enchant_skill, 4), AD_ELEC);
+
+                    if (resists_elec(mon) || defended(mon, AD_ELEC)) {
+                        shieldeff(mon->mx, mon->my);
+                        golemeffects(mon, AD_ELEC, tmp);
+                        if (!Blind)
+                            Your("%s don't shock %s!",
+                                 is_bird(youmonst.data)
+                                   ? "claws" : makeplural(body_part(HAND)),
+                                 mon_nam(mon));
+                    } else {
+                        You("%s %s with your %s!",
+                            vulnerable_to(mon, AD_ELEC)
+                              ? "severely shock"
+                              : rn2(2)
+                                ? "jolt" : "shock",
+                            mon_nam(mon),
+                            is_bird(youmonst.data)
+                              ? "claws" : makeplural(body_part(HAND)));
+
+                        if (!rn2(4))
+                            (void) destroy_mitem(mon, WAND_CLASS, AD_ELEC);
+                        if (!rn2(5))
+                            (void) destroy_mitem(mon, RING_CLASS, AD_ELEC);
+
+                        sum[i] = damageum(mon, mattk, shocking_grasp);
+                    }
+                    nohandshock();
                 }
             } else { /* !dhit */
                 missum(mon, tmp, dieroll, mattk, (tmp + armorpenalty > dieroll));
@@ -4518,7 +4614,7 @@ boolean weapon_attacks; /* skip weapon attacks if false */
                  || youmonst.data->mlet == S_GNOME) && !weapon_used)
                 goto use_weapon;
             sum[i] = castum(mon, mattk);
-             		continue;
+            continue;
             /*FALLTHRU*/
 
         case AT_NONE:
@@ -5442,14 +5538,19 @@ nohandburn()
         return;
     if (u.umburn == 1) {
         if (Blind)
-            Your("%s no longer feel hot.", hands);
+            Your("%s no longer feel hot.",
+                 is_bird(youmonst.data) ? "claws" : hands);
         else
-            Your("%s stop burning %s.", hands, hcolor(NH_ORANGE));
+            Your("%s stop burning %s.",
+                 is_bird(youmonst.data) ? "claws" : hands,
+                 hcolor(NH_ORANGE));
     } else {
         if (Blind)
-            pline_The("burning sensation in your %s lessens.", hands);
+            pline_The("burning sensation in your %s lessens.",
+                      is_bird(youmonst.data) ? "claws" : hands);
         else
-            Your("%s no longer burn so brightly.", hands);
+            Your("%s no longer burn so brightly.",
+                 is_bird(youmonst.data) ? "claws" : hands);
     }
     u.umburn--;
 }
@@ -5463,14 +5564,19 @@ nohandshock()
         return;
     if (u.umshock == 1) {
         if (Blind)
-            Your("%s no longer feel energized.", hands);
+            Your("%s no longer feel energized.",
+                 is_bird(youmonst.data) ? "claws" : hands);
         else
-            Your("%s stop shimmering %s.", hands, hcolor(NH_BLUE));
+            Your("%s stop shimmering %s.",
+                 is_bird(youmonst.data) ? "claws" : hands,
+                 hcolor(NH_BLUE));
     } else {
         if (Blind)
-            pline_The("energetic sensation in your %s lessens.", hands);
+            pline_The("energetic sensation in your %s lessens.",
+                      is_bird(youmonst.data) ? "claws" : hands);
         else
-            Your("%s no longer shimmer so brightly.", hands);
+            Your("%s no longer shimmer so brightly.",
+                 is_bird(youmonst.data) ? "claws" : hands);
     }
     u.umshock--;
 }

include/mondata.h
@@ -310,6 +310,7 @@
 #define has_trunk(ptr) \
     ((ptr) == &mons[PM_MUMAK] || (ptr) == &mons[PM_MASTODON] \
      || (ptr) == &mons[PM_WOOLLY_MAMMOTH])
+#define is_quadruped(ptr) ((ptr)->mlet ==S_QUADRUPED)
 #define is_golem(ptr) ((ptr)->mlet == S_GOLEM)
 #define is_ent(ptr) ((ptr)->mlet == S_ENT)
 #define is_plant(ptr) ((ptr)->mlet == S_PLANT)

src/mhitm.c
@@ -887,11 +887,14 @@ int dieroll;
             Strcpy(magr_name, Monnam(magr));
             switch (mattk->aatyp) {
             case AT_BITE:
-		Sprintf(buf,"%s %ss", magr_name, has_beak(magr->data) ?
-                        "peck" : "bite");
+		Sprintf(buf,"%s %ss", magr_name,
+                        has_beak(magr->data)
+                           ? "peck" : "bite");
                 break;
             case AT_KICK:
-                Sprintf(buf, "%s kicks", magr_name);
+                Sprintf(buf, "%s %s", magr_name,
+                        is_quadruped(magr->data)
+                          ? "tramples" : "kicks");
                 break;
             case AT_STNG:
                 Sprintf(buf, "%s stings", magr_name);

src/mhitu.c
@@ -65,7 +65,9 @@ struct attack *mattk;
                   "peck" : "bite");
             break;
         case AT_KICK:
-            pline("%s kicks%c", Monst_name,
+            pline("%s %s%c", Monst_name,
+                  is_quadruped(mtmp->data)
+                    ? "tramples you" : "kicks",
                   (thick_skinned(youmonst.data)
                    || Barkskin || Stoneskin) ? '.' : '!');
             break;

src/monst.c
@@ -994,14 +994,14 @@ NEARDATA struct permonst mons[] = {
      * quadrupeds
      */
     MON("rothe", S_QUADRUPED, LVL(2, 9, 7, 0, 0), (G_GENO | G_SGROUP | 4),
-        A(ATTK(AT_CLAW, AD_PHYS, 1, 3), ATTK(AT_BITE, AD_PHYS, 1, 3),
-          ATTK(AT_BITE, AD_PHYS, 1, 8), NO_ATTK, NO_ATTK, NO_ATTK),
+        A(ATTK(AT_BITE, AD_PHYS, 1, 3), ATTK(AT_KICK, AD_PHYS, 1, 3),
+          ATTK(AT_BUTT, AD_PHYS, 1, 8), NO_ATTK, NO_ATTK, NO_ATTK),
         SIZ(400, 100, MS_ROAR, MZ_LARGE), 0, 0,
         M1_ANIMAL | M1_NOHANDS | M1_OMNIVORE, M2_HOSTILE | M2_DRUID_FORM_A,
         M3_INFRAVISIBLE, 0, 0, 4, CLR_BROWN),
     MON("leocrotta", S_QUADRUPED, LVL(6, 18, 4, 10, 0), (G_GENO | 2),
-        A(ATTK(AT_CLAW, AD_PHYS, 2, 6), ATTK(AT_BITE, AD_PHYS, 2, 6),
-          ATTK(AT_CLAW, AD_PHYS, 2, 6), NO_ATTK, NO_ATTK, NO_ATTK),
+        A(ATTK(AT_BITE, AD_PHYS, 2, 6), ATTK(AT_KICK, AD_PHYS, 2, 6),
+          ATTK(AT_KICK, AD_PHYS, 2, 6), NO_ATTK, NO_ATTK, NO_ATTK),
         SIZ(1200, 500, MS_IMITATE, MZ_LARGE), 0, 0,
         M1_ANIMAL | M1_NOHANDS | M1_OMNIVORE, M2_HOSTILE | M2_STRONG,
         M3_INFRAVISIBLE, 0, 0, 8, CLR_RED),
@@ -1032,13 +1032,13 @@ NEARDATA struct permonst mons[] = {
         M2_HOSTILE | M2_STRONG | M2_DRUID_FORM_C,
         M3_INFRAVISIBLE | M3_BERSERK, 0, 0, 13, CLR_YELLOW),
     MON("titanothere", S_QUADRUPED, LVL(12, 12, 6, 0, 0), (G_GENO | 2),
-        A(ATTK(AT_CLAW, AD_PHYS, 2, 8), NO_ATTK, NO_ATTK, NO_ATTK, NO_ATTK,
+        A(ATTK(AT_BUTT, AD_PHYS, 2, 8), NO_ATTK, NO_ATTK, NO_ATTK, NO_ATTK,
           NO_ATTK),
         SIZ(2650, 650, MS_SILENT, MZ_HUGE), 0, 0,
         M1_ANIMAL | M1_THICK_HIDE | M1_NOHANDS | M1_HERBIVORE,
         M2_HOSTILE | M2_STRONG, M3_INFRAVISIBLE, 0, 0, 13, CLR_GRAY),
     MON("baluchitherium", S_QUADRUPED, LVL(14, 12, 5, 0, 0), (G_GENO | 2),
-        A(ATTK(AT_CLAW, AD_PHYS, 5, 4), ATTK(AT_CLAW, AD_PHYS, 5, 4), NO_ATTK,
+        A(ATTK(AT_KICK, AD_PHYS, 5, 4), ATTK(AT_KICK, AD_PHYS, 5, 4), NO_ATTK,
           NO_ATTK, NO_ATTK, NO_ATTK),
         SIZ(3800, 800, MS_SILENT, MZ_HUGE), 0, 0,
         M1_ANIMAL | M1_THICK_HIDE | M1_NOHANDS | M1_HERBIVORE,

src/read.c
@@ -1498,11 +1498,13 @@ struct obj *sobj; /* sobj - scroll or fake spellbook for spell */
     case SPE_BURNING_HANDS:
         if (u.umshock) {
             Your("%s are already energized.",
-                 makeplural(body_part(HAND)));
+                 is_bird(youmonst.data)
+                   ? "claws" : makeplural(body_part(HAND)));
             break;
         } else if (u.uinwater) {
             Your("%s glow for a brief moment, then return to normal.",
-                 makeplural(body_part(HAND)));
+                 is_bird(youmonst.data)
+                   ? "claws" : makeplural(body_part(HAND)));
             break;
         } else if (confused) {
             /* can't cast spells when confused, but including this
@@ -1510,10 +1512,14 @@ struct obj *sobj; /* sobj - scroll or fake spellbook for spell */
                be read */
             if (!sblessed) {
                 if (Blind)
-                    Your("%s feel warm.", makeplural(body_part(HAND)));
+                    Your("%s feel warm.",
+                         is_bird(youmonst.data)
+                           ? "claws" : makeplural(body_part(HAND)));
                 else
                     Your("%s are engulfed in a %s flame.",
-                         makeplural(body_part(HAND)), hcolor(NH_PURPLE));
+                         is_bird(youmonst.data)
+                           ? "claws" : makeplural(body_part(HAND)),
+                         hcolor(NH_PURPLE));
                 if (how_resistant(FIRE_RES) < 100)
                     losehp(1, "burning hands", KILLED_BY);
             } else {
@@ -1528,19 +1534,25 @@ struct obj *sobj; /* sobj - scroll or fake spellbook for spell */
         } else {
             if (!sblessed) {
                 if (Blind)
-                    Your("%s feel hot.", makeplural(body_part(HAND)));
+                    Your("%s feel hot.",
+                         is_bird(youmonst.data)
+                           ? "claws" : makeplural(body_part(HAND)));
                 else
                     Your("%s are engulfed in a%s%s flame.",
-                         makeplural(body_part(HAND)),
+                         is_bird(youmonst.data)
+                           ? "claws" : makeplural(body_part(HAND)),
                          u.umburn ? " bright " : "n ", hcolor(NH_ORANGE));
                 u.umburn++;
             } else {
                 if (Blind)
-                    Your("%s feel %s hot.", makeplural(body_part(HAND)),
+                    Your("%s feel %s hot.",
+                         is_bird(youmonst.data)
+                           ? "claws" : makeplural(body_part(HAND)),
                          u.umburn ? "extremely" : "very");
                 else
                     Your("%s are engulfed in a %s %s flame.",
-                         makeplural(body_part(HAND)),
+                         is_bird(youmonst.data)
+                           ? "claws" : makeplural(body_part(HAND)),
                          u.umburn ? "searing" : "brilliant", hcolor(NH_ORANGE));
                 /* after a while, repeated uses become less effective */
                 if (u.umburn >= 20)
@@ -1554,7 +1566,8 @@ struct obj *sobj; /* sobj - scroll or fake spellbook for spell */
     case SPE_SHOCKING_GRASP:
         if (u.umburn) {
             Your("%s are already engulfed in flame.",
-                 makeplural(body_part(HAND)));
+                 is_bird(youmonst.data)
+                   ? "claws" : makeplural(body_part(HAND)));
             break;
         } else if (confused) {
             /* can't cast spells when confused, but including this
@@ -1562,10 +1575,14 @@ struct obj *sobj; /* sobj - scroll or fake spellbook for spell */
                be read */
             if (!sblessed) {
                 if (Blind)
-                    Your("%s feel numb.", makeplural(body_part(HAND)));
+                    Your("%s feel numb.",
+                         is_bird(youmonst.data)
+                           ? "claws" : makeplural(body_part(HAND)));
                 else
                     Your("%s are surrounded by a %s aura.",
-                         makeplural(body_part(HAND)), hcolor(NH_PURPLE));
+                         is_bird(youmonst.data)
+                           ? "claws" : makeplural(body_part(HAND)),
+                         hcolor(NH_PURPLE));
                 if (how_resistant(SHOCK_RES) < 100)
                     losehp(1, "shocking grasp", KILLED_BY);
             } else {
@@ -1580,19 +1597,25 @@ struct obj *sobj; /* sobj - scroll or fake spellbook for spell */
         } else {
             if (!sblessed) {
                 if (Blind)
-                    Your("%s feel energized.", makeplural(body_part(HAND)));
+                    Your("%s feel energized.",
+                         is_bird(youmonst.data)
+                           ? "claws" : makeplural(body_part(HAND)));
                 else
                     Your("%s are surrounded by a %s%s aura.",
-                         makeplural(body_part(HAND)),
+                         is_bird(youmonst.data)
+                           ? "claws" : makeplural(body_part(HAND)),
                          u.umshock ? "bright " : "", hcolor(NH_BLUE));
                 u.umshock++;
             } else {
                 if (Blind)
-                    Your("%s feel %s energized.", makeplural(body_part(HAND)),
+                    Your("%s feel %s energized.",
+                         is_bird(youmonst.data)
+                           ? "claws" : makeplural(body_part(HAND)),
                          u.umshock ? "extremely" : "very");
                 else
                     Your("%s are surrounded by a%s %s aura.",
-                         makeplural(body_part(HAND)),
+                         is_bird(youmonst.data)
+                           ? "claws" : makeplural(body_part(HAND)),
                          u.umshock ? "n energetic" : " brilliant", hcolor(NH_BLUE));
                 /* after a while, repeated uses become less effective */
                 if (u.umshock >= 20)

src/trap.c
@@ -4663,7 +4663,9 @@ drown()
             You("sink like %s.", Hallucination ? "the Titanic" : "a rock");
         if (u.umburn) {
             u.umburn = 0;
-            Your("%s are extinguished.", makeplural(body_part(HAND)));
+            Your("%s are extinguished.",
+                 is_bird(youmonst.data)
+                   ? "claws" : makeplural(body_part(HAND)));
         }
     }
 

commit d356b616e13fd929ffd7ac7ed354f51c184b9f6f

Fix: dmonsfree killing monster using burning hands/shocking grasp while polymorphed.
If using the spells burning hands or shocking grasp, and polymorphed
into a monster that has a claw/touch/kick attack, an extra attack was
snuck into the order of events, and could cause a dmonsfree error if the
target monster was killed via one of these erroneous attacks. This bug
was introduced from commit aea1e41 a few months ago. I used sum[i] here
for the damage instead of just calling the damage directly. Oops.

src/uhitm.c
@@ -4451,7 +4451,6 @@ boolean weapon_attacks; /* skip weapon attacks if false */
                                          ? 4 : (P_SKILL(P_ENCHANTMENT_SPELL) == P_SKILLED)
                                            ? 3 : (P_SKILL(P_ENCHANTMENT_SPELL) == P_BASIC)
                                              ? 2 : 1);
-                    int burning_hands = damage_mon(mon, d(enchant_skill, 4), AD_FIRE, TRUE);
 
                     if (resists_fire(mon) || defended(mon, AD_FIRE)
                         || mon_underwater(mon)) {
@@ -4496,7 +4495,7 @@ boolean weapon_attacks; /* skip weapon attacks if false */
                             pline("%s ignites and turns to ash!", Monnam(mon));
                             killed(mon);
                         } else {
-                            sum[i] = damageum(mon, mattk, burning_hands);
+                            damage_mon(mon, d(enchant_skill, 4), AD_FIRE, TRUE);
                         }
                     }
                     nohandburn();
@@ -4509,7 +4508,6 @@ boolean weapon_attacks; /* skip weapon attacks if false */
                                          ? 4 : (P_SKILL(P_ENCHANTMENT_SPELL) == P_SKILLED)
                                            ? 3 : (P_SKILL(P_ENCHANTMENT_SPELL) == P_BASIC)
                                              ? 2 : 1);
-                    int shocking_grasp = damage_mon(mon, d(enchant_skill, 4), AD_ELEC, TRUE);
 
                     if (resists_elec(mon) || defended(mon, AD_ELEC)) {
                         shieldeff(mon->mx, mon->my);
@@ -4534,7 +4532,7 @@ boolean weapon_attacks; /* skip weapon attacks if false */
                         if (!rn2(5))
                             (void) destroy_mitem(mon, RING_CLASS, AD_ELEC);
 
-                        sum[i] = damageum(mon, mattk, shocking_grasp);
+                        damage_mon(mon, d(enchant_skill, 4), AD_ELEC, TRUE);
                     }
                     nohandshock();
                 }