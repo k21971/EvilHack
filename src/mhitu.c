k21971/EvilHack

commit 307d3992502b380f9af3e3c62aa5e4f5d9b4c3ab

Fix: super tame steeds attacking monsters they shouldn't while being ridden.
A while ago I set a condition to where if your steed had a tameness
value of 15 or higher, it would attack hostiles on your behalf instead
of waiting to be attacked first. Unfortunately this bypassed
acceptable_pet_target(), meaning your awesome super tame warhorse would
decide to kick that cockatrice for the lulz. This commit addresses that.
FYI, if it's directly attacked, your steed will fight back.

src/mhitu.c
@@ -616,8 +616,12 @@ register struct monst *mtmp;
             /* Let your steed retaliate */
             return !!(mattackm(u.usteed, mtmp) & MM_DEF_DIED);
         }
-        if (u.usteed->mtame >= 15)
-            return !!(mattackm(u.usteed, mtmp) & MM_DEF_DIED);
+        if (u.usteed->mtame >= 15) {
+            if (!acceptable_pet_target(u.usteed, mtmp, FALSE))
+                return 0;
+            else
+                return !!(mattackm(u.usteed, mtmp) & MM_DEF_DIED);
+        }
     }
 
     if (u.uundetected && !range2 && foundyou && !u.uswallow) {

commit b41949570ff3fc24590cc0a7f5d79198cf0f704f

Fix: regression from commit 307d399 (loyal+ steeds attacking monsters they shouldn't while being ridden).
Almost five years ago I had added a check against
acceptable_pet_target() for loyal+ ridden steeds so they wouldn't attack
monsters they shouldn't, like a cockatrice (touching it petrifies the
unprotected attacker). At some point this was removed when fixing up
this section of code. I haven't traced exactly when this happened, but
it would have to have been when 0.9.2 was released. Anyways, placing the
check back in, as well as checking in another spot, AND while here,
noticed that the player couldn't cast the spell 'stone to flesh' on
their steed while riding it, so I addressed that as well.

src/mhitu.c
@@ -712,7 +712,10 @@ struct monst *mtmp;
             if ((i & MM_DEF_DIED) != 0 || !u.usteed
                 || distu(mtmp->mx, mtmp->my) > 2)
                 return MM_MISS;
-            /* Let your steed retaliate */
+            /* Let your steed retaliate (check acceptable_pet_target()
+               as a guard) */
+            if (!acceptable_pet_target(u.usteed, mtmp, FALSE))
+                return MM_MISS;
             bhitpos.x = mtmp->mx, bhitpos.y = mtmp->my;
             notonhead = FALSE;
             return (mattackm(u.usteed, mtmp) & MM_DEF_DIED) ? MM_AGR_DIED : MM_HIT;
@@ -722,7 +725,8 @@ struct monst *mtmp;
            is loyal or greater. if the steed has a ranged attack, it
            can utilize it apart from this routine */
         if (u.usteed->mtame >= 15
-            && distu(mtmp->mx, mtmp->my) <= 2) {
+            && distu(mtmp->mx, mtmp->my) <= 2
+            && acceptable_pet_target(u.usteed, mtmp, FALSE)) {
             /* steed attacks without provocation,
                make sure it's actually alive */
             i = mattackm(u.usteed, mtmp);

src/zap.c
@@ -3518,6 +3518,7 @@ struct obj *obj; /* wand or spell */
     /* Default processing via bhitm() for these */
     case SPE_CURE_SICKNESS:
     case SPE_RESTORE_ABILITY:
+    case SPE_STONE_TO_FLESH:
     case WAN_MAKE_INVISIBLE:
     case WAN_CANCELLATION:
     case SPE_CANCELLATION: