be3d248a4fd56cf789270a8232c62def5a1fbe9a
nhmall <nhmall@nethack.org>
2026-02-09 08:39:21 -0500

    avoid a reported obuf reuse in use_misc()
    Report: https://github.com/NetHack/NetHack/issues/1486
    
    Resolves #1486

--- src/muse.c
+++ src/muse.c
@@ -1914,7 +1914,7 @@ struct monst *mtmp;
             int where_to = rn2(4);
             struct obj *obj = uwep;
             const char *hand;
-            char the_weapon[BUFSZ];
+            char the_weapon[BUFSZ], hand_buf[BUFSZ];
 
             if (!obj || !canletgo(obj, "")
                 || (u.twoweap && canletgo(uswapwep, "") && rn2(2)))
@@ -1926,10 +1926,12 @@ struct monst *mtmp;
             hand = body_part(HAND);
             if (bimanual(obj))
                 hand = makeplural(hand);
+            (void) strncpy(hand_buf, hand, sizeof hand_buf - 1);
+            hand_buf[sizeof hand_buf - 1] = '\0';
 
             if (vismon)
                 pline("%s flicks a bullwhip towards your %s!", Monnam(mtmp),
-                      hand);
+                      hand_buf);
             if (obj->otyp == HEAVY_IRON_BALL) {
                 pline("%s fails to wrap around %s.", The_whip, the_weapon);
                 return 1;
@@ -1938,7 +1940,7 @@ struct monst *mtmp;
                   the_weapon);
             if (welded(obj)) {
                 pline("%s welded to your %s%c",
-                      !is_plural(obj) ? "It is" : "They are", hand,
+                      !is_plural(obj) ? "It is" : "They are", hand_buf,
                       !obj->bknown ? '!' : '.');
                 /* obj->bknown = 1; */ /* welded() takes care of this */
                 where_to = 0;
@@ -1957,7 +1959,7 @@ struct monst *mtmp;
             switch (where_to) {
             case 1: /* onto floor beneath mon */
                 pline("%s yanks %s from your %s!", Monnam(mtmp), the_weapon,
-                      hand);
+                      hand_buf);
                 place_object(obj, mtmp->mx, mtmp->my);
                 break;
             case 2: /* onto floor beneath you */
